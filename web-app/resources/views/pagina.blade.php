0994fc64a42bb0cf71377de4349837c39ac138af
Ingrid-st <ingridfarias161@gmail.com>
2021-10-08 20:39:28 -0300

    Implementando Start Kit Breeze

---
+++ web-app/resources/views/pagina.blade.php
@@ -0,0 +1,19 @@
+    <!doctype html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Pagina</title>
+        </heade>
+        <body>
+            <h1>Séries Favoritas:</h1>
+            <ul>
+                <li>Stranger Things</li>
+                <li>Teen Wolf</li>
+                <li>The Vampires Diares</li>
+                <li>Dark</li>
+                <li>Friends</li>
+            </ul>
+        </body>
+    </html>
